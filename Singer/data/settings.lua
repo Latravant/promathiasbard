e90add250aab1cdd2c98cc0f47be1fc3e5966429
Latravant <latravant@gmail.com>
2022-04-09 09:23:20 -0400

    Add files via upload

---
+++ Singer/data/settings.lua
@@ -0,0 +1,22 @@
+return {
+    ["dummy"] = L{"Knight's Minne","Knight's Minne II",},
+    ["debuffs"] = L{"Carnage Elegy","Pining Nocturne",},
+    ["playlist"] = T{
+        ["dpsonly"] = L{"Advancing March","Victory March","Blade Madrigal","Sword Madrigal","Valor Minuet V",},
+        ["dps1"] = L{"Advancing March","Victory March","Blade Madrigal","Sword Madrigal","Valor Minuet V",},
+        ["dd"] = L{"Advancing March","Victory March","Blade Madrigal","Sword Madrigal","Valor Minuet V",},
+        ["dps2"] = L{"Advancing March","Victory March","Blade Madrigal","Sword Madrigal","Valor Minuet V",},
+        ["apex"] = L{"Mage's Ballad II","Mage's Ballad III","Advancing March","Victory March","Blade Madrigal",},
+        ["clear"] = L{},
+        ["2dd"] = L{"Advancing March","Victory March","Blade Madrigal","Sword Madrigal","Valor Minuet V",},
+    },
+    ["buffs"] = T{
+        ["refresh"] = L{"Off",},
+        ["haste"] = L{},
+        ["aurorastorm"] = L{},
+        ["firestorm"] = L{},
+    },
+    ["song"] = T{
+    },
+    ["songs"] = L{"Advancing March","Victory March","Blade Madrigal","Sword Madrigal","Valor Minuet V",},
+}
